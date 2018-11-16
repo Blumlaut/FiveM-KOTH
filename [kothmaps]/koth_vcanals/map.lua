2bfc0c620bbc19e47e15ce15c2ec85350c0a9aab
Eric Mager <iamabluewolf@gmail.com>
2018-11-16 15:25:36 +0100

    add koth_vcanals

---
+++ [kothmaps]/koth_vcanals/map.lua
@@ -0,0 +1,38 @@
+koth {-960.5,-1091.11,2.15,45.0} -- koth area, x,y,z,r 
+
+teams {
+	{"red", {255,0,0,1}},
+	{"green", {0,255,0,2}},
+	{"blue", {0,0,255,3}}
+}
+
+zoneconfig {
+	zoneBlip = 9,
+	zoneAlpha = 100,
+	zoneColor = 4,
+	blip = 305,
+	blipcolor = 4,
+	blipalpha = 255
+}
+
+shops {
+	{-1014.25,-1167.5,2.15},
+	{-970.66,-1011.94,2.15},
+	{-897.2,-1066.48,2.15}
+}
+
+spawns {
+	{-975.22,-1014.5,2.15}, --team 1
+	{-1020.66,-1173.28,2.15},  --team 2 
+	{-902.2,-1059.8,2.15}   --team 3
+}
+
+
+vehicleSpawns { -- can be multiple per team
+	{ -- team 1
+	},
+	{ -- team 2
+	},
+	{ -- team 3
+	}
+}
