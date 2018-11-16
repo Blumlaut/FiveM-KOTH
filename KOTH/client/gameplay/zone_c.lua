033c8e36f9ee41e6159c3d98e51021de7543ff73
Eric Mager <iamabluewolf@gmail.com>
2018-11-16 15:33:33 +0100

    fix map changing AGAIN

--- KOTH/client/gameplay/zone_c.lua
+++ KOTH/client/gameplay/zone_c.lua
@@ -24,63 +24,60 @@ AddEventHandler("SetGameFinished", function(teamid) -- todo, add ranking ui or s
 
 end)
 
-function createZone(zone)
-	Citizen.CreateThread(function()
-		while true do
-			if changingMap == true then break end -- Can it be any more hacky then this?
-			Wait(100)
-			local x,y,z = table.unpack(GetEntityCoords(PlayerPedId(), true))
-			local dist = GetDistanceBetweenCoords(x, y, z, zone.x, zone.y, zone.z, false)
-			
-			if dist < zone.r-math.pi and not isPlayerInZone and not IsPlayerDead(PlayerId()) then
-				TriggerServerEvent("PlayerEnteredKothZone",Teaminfo.id)
-				Citizen.Trace("entered warzone")
-				isPlayerInZone = true
-			elseif dist > zone.r-math.pi and isPlayerInZone then
-				TriggerServerEvent("PlayerLeftKothZone",Teaminfo.id)
-				isPlayerInZone = false
-			elseif isPlayerInZone and IsPlayerDead(PlayerId()) then
-				TriggerServerEvent("PlayerLeftKothZone",Teaminfo.id)
-				isPlayerInZone = false 
-			end
+Citizen.CreateThread(function()
+	while true do
+		if changingMap == true then break end -- Can it be any more hacky then this?
+		Wait(100)
+		local x,y,z = table.unpack(GetEntityCoords(PlayerPedId(), true))
+		local dist = GetDistanceBetweenCoords(x, y, z, zone.x, zone.y, zone.z, false)
+		
+		if dist < zone.r-math.pi and not isPlayerInZone and not IsPlayerDead(PlayerId()) then
+			TriggerServerEvent("PlayerEnteredKothZone",Teaminfo.id)
+			Citizen.Trace("entered warzone")
+			isPlayerInZone = true
+		elseif dist > zone.r-math.pi and isPlayerInZone then
+			TriggerServerEvent("PlayerLeftKothZone",Teaminfo.id)
+			isPlayerInZone = false
+		elseif isPlayerInZone and IsPlayerDead(PlayerId()) then
+			TriggerServerEvent("PlayerLeftKothZone",Teaminfo.id)
+			isPlayerInZone = false 
 		end
-	end)
-end
+	end
+end)
 
 function createBlip(zone)
-	Citizen.CreateThread(function()
-		zoneArea = AddBlipForRadius(zone.x, zone.y, zone.z, zone.r+.0) --Creates the circle on the map
-		zoneBlip = AddBlipForCoord(zone.x, zone.y, zone.z) -- Creates the name of the blip on the list in the pause menu
-		SetBlipSprite(zoneArea, 9)
-		SetBlipAlpha(zoneArea, 100)
-		SetBlipColour(zoneArea, 4)
-		SetBlipSprite(zoneBlip, 305)
-		SetBlipColour(zoneBlip, 4)
-		SetBlipAlpha(zoneBlip, 255)
-		BeginTextCommandSetBlipName("STRING")
-		AddTextComponentString("Warzone")
-		EndTextCommandSetBlipName(zoneBlip)	
-
-		AddEventHandler("SetZoneOwner", function(teamid)
-			if teamid then
-				SetBlipColour(zoneArea, mapData.teams[teamid][2][4])
-				SetBlipColour(zoneBlip, mapData.teams[teamid][2][4])
-			else
-				SetBlipColour(zoneArea, 4)
-				SetBlipColour(zoneBlip, 4)
-			end
-		end)
-	end)
+	zoneArea = AddBlipForRadius(zone.x, zone.y, zone.z, zone.r+.0) --Creates the circle on the map
+	zoneBlip = AddBlipForCoord(zone.x, zone.y, zone.z) -- Creates the name of the blip on the list in the pause menu
+	SetBlipSprite(zoneArea, 9)
+	SetBlipAlpha(zoneArea, 100)
+	SetBlipColour(zoneArea, 4)
+	SetBlipSprite(zoneBlip, 305)
+	SetBlipColour(zoneBlip, 4)
+	SetBlipAlpha(zoneBlip, 255)
+	BeginTextCommandSetBlipName("STRING")
+	AddTextComponentString("Warzone")
+	EndTextCommandSetBlipName(zoneBlip)	
 end
 
-AddEventHandler("changeMap",function(zone) -- Changing blip and zone here
+AddEventHandler("SetZoneOwner", function(teamid)
+	if teamid then
+		SetBlipColour(zoneArea, mapData.teams[teamid][2][4])
+		SetBlipColour(zoneBlip, mapData.teams[teamid][2][4])
+	else
+		SetBlipColour(zoneArea, 4)
+		SetBlipColour(zoneBlip, 4)
+	end
+end)
+
+AddEventHandler("changeMap",function(nzone) -- Changing blip and zone here
 	print("changing map")
 	if zoneArea ~= nil then
 		RemoveBlip(zoneArea)
 		RemoveBlip(zoneBlip)
 	end
-	changeMap = true
+	changingMap = true
+	zone = nzone
 	Citizen.Wait(120)
-	createZone(zone)
-	createBlip(zone)
+	createBlip(nzone)
+	changingMap = false
 end)
