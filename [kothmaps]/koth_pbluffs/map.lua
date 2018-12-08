19b4d9323b4d716356547e62507cdc078b2120ac
Eric Mager <iamabluewolf@gmail.com>
2018-12-08 16:03:43 +0100

    add a good-er map

---
+++ [kothmaps]/koth_pbluffs/map.lua
@@ -0,0 +1,38 @@
+koth {-2249.6,271.7,179.30,45.0} -- koth area, x,y,z,r 
+
+teams {
+	{"Red", {255,0,0,1}},
+	{"Green", {0,255,0,2}},
+	{"Blue", {0,0,255,3}}
+}
+
+zoneconfig {
+	zoneBlip = 9,
+	zoneAlpha = 100,
+	zoneColor = 4,
+	blip = 305,
+	blipcolor = 4,
+	blipalpha = 255
+}
+
+shops {
+	{-2257.5,166.8,174.0},
+	{-2338.8,240.9,169.6},
+	{-2249.4,387.4,174.6}
+}
+
+spawns {
+	{-2240.9,167.42,174.6}, --team 1
+	{-2251.9,396.6,174.6},  --team 2 
+	{-2343.05,267.55,169.46}   --team 3
+}
+
+
+vehicleSpawns { -- can be multiple per team
+	{ -- team 1
+	},
+	{ -- team 2
+	},
+	{ -- team 3
+	}
+}
